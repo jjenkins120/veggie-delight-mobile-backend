34a40299128943f1c0077200b2ff817660af24be
github username <github email address>
2021-01-13 17:34:12 -0600

    namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,8 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# namespace :api do
+#   namespace :v1 do
+#     post '/auth', to: 'auth#create'
+#       get '/current_user', to: 'auth#show'
