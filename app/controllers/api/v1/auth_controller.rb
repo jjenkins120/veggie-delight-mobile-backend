8271e0865129701c7b846e50f07f968ecc18add5
jjenkins120 <jjenkins120@gmail.com>
2021-01-22 16:27:31 -0600

    added id field to user params

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,9 +3,9 @@ require 'dotenv-rails'
 
 class Api::V1::AuthController < ApplicationController
 
-    def hmac_secret
-        ENV["API_SECRET_KEY"]
-    end
+    # def hmac_secret
+    #     ENV["API_SECRET_KEY"]
+    # end
     # The hmac_secret method just gets and returns our secret from our environment
 
     def create
@@ -13,7 +13,7 @@ class Api::V1::AuthController < ApplicationController
         
         if user && user.authenticate(params[:password])
             payload = {user_id: user.id}
-            token = JWT.encode(payload, hmac_secret, 'HS256')
+            token = JWT.encode(payload, 'S3cr3t', 'HS256')
             # render json: {id: user.id, email: user.email, token: token}
             render json: {id: user.id, token: token}
         else
@@ -23,7 +23,7 @@ class Api::V1::AuthController < ApplicationController
 
     def show
         token = request.headers[:Authorization].split(' ').last  
-        decoded_token = JWT.decode(token, hmac_secret, true, { algorithm: 'HS256'} )
+        decoded_token = JWT.decode(token, 'S3cr3t', true, { algorithm: 'HS256'} )
         user_id = decoded_token[0]['user_id']
         user = User.find(user_id)
 
